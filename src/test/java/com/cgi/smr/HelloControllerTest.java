8d2dfcfed51e2f76c5f332c1e3c27b7edcbf2a0c
Michael Carvalho <michael.carvalho@cgi.com>
2017-07-13 23:37:43 +0100

    Structuring Project

--- src/test/java/com/cgi/smr/HelloControllerTest.java
+++ src/test/java/com/cgi/smr/HelloControllerTest.java
@@ -19,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class HelloControllerTest {
 
+
     @Autowired
     private MockMvc mvc;
 
