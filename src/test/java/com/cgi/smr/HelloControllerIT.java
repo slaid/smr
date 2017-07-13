cf70e69c315ffc1165539bfe92bfdf3875385410
Michael Carvalho <michael.carvalho@cgi.com>
2017-07-13 23:35:49 +0100

    Structuring Project

---
+++ src/test/java/com/cgi/smr/HelloControllerIT.java
@@ -0,0 +1,40 @@
+package com.cgi.smr;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URL;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> responseEntity = template.getForEntity(base.toString(), String.class);
+        assertThat(responseEntity.getBody(), equalTo("Greetings from Spring Boot!"));
+    }
+}
