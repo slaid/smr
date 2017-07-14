8e24e09ede7be8b21fb3e9f3ab14c3571d81c165
Michael Carvalho <michael.carvalho@cgi.com>
2017-07-14 17:06:25 +0100

    Structuring Project

--- src/main/java/com/cgi/smr/Application.java
+++ src/main/java/com/cgi/smr/Application.java
@@ -1,20 +1,58 @@
 package com.cgi.smr;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return args -> {
+            // save a couple
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer: repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // Fetch an Individual Customer by ID
+            Customer customer = repository.findOne(1L);
+            log.info("Customer found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
+
+            // Fetch Customers by Last Name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            for (Customer bauer: repository.findByLastName("Bauer")) {
+                log.info(bauer.toString());
+            }
+            log.info("");
+        };
+    }
+
+    /*
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
@@ -25,5 +63,6 @@ public class Application {
                 System.out.println(beanName);
         };
     }
+    */
 
 }
---
+++ src/test/java/com/cgi/smr/HelloControllerTest.java
@@ -0,0 +1,25 @@
+package com.cgi.smr;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+
+}
